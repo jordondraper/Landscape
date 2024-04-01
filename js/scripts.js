3b5dee634f6aef4294f9e8ce1a1762bdcc44e149
Jordon Draper <jordondraper17@gmail.com>
2024-03-31 23:18:22 -0600

    Submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,29 @@
 // inject current year in footer
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
+
+const hb = document.querySelector('#hamburgerBtn');
+
+hb.addEventListener('click', () => {
+    document.querySelector('#primaryNav').classList.toggle('open');
+})
+
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    effect: 'slide',
+    speed: 2000,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    //start Autoplay
+
+    autoplay: {
+        delay: 5000,
+    },
+  });
